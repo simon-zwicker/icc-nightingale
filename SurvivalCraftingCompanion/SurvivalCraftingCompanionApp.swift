f2b6f1ad755e5e2d6d109991f51a4c9359b21301
Simon Zwicker <s.zwicker@appdev.de>
2024-03-19 10:17:38 +0100

    Initial Commit

---
+++ SurvivalCraftingCompanion/SurvivalCraftingCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurvivalCraftingCompanionApp.swift
+//  SurvivalCraftingCompanion
+//
+//  Created by Simon Zwicker on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SurvivalCraftingCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
