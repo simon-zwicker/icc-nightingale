bbe6a56c4ea97059c5a5ca2297b9122a5c169f3c
Simon Zwicker <s.zwicker@appdev.de>
2024-03-21 14:07:23 +0100

    changed stuff

--- SurvivalCraftingCompanion/App/Items/ItemsView.swift
+++ SurvivalCraftingCompanion/App/Items/ItemsView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import SwiftUIGlass
 
 struct ItemsView: View {
 
--- SurvivalCraftingCompanion/Utils/Extensions/View.swift
+++ SurvivalCraftingCompanion/Utils/Extensions/View.swift
@@ -9,28 +9,4 @@ import SwiftUI
 
 extension View {
 
-    func glass(
-        cornerRadius: CGFloat,
-        corners: UIRectCorner? = .allCorners,
-        fill: Color = .white,
-        opacity: CGFloat = 0.25,
-        shadowRadius: CGFloat = 10.0
-    ) -> some View {
-        modifier(
-            GlassModifier(
-                cornerRadius: cornerRadius, 
-                corners: corners,
-                fill: fill,
-                opacity: opacity,
-                shadowRadius: shadowRadius)
-        )
-    }
-
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape(RoundedCorner(radius: radius, corners: corners))
-    }
-
-    func glassCell(cornerRadius: CGFloat, corners: UIRectCorner? = nil) -> some View {
-        modifier(GlassCellModifier(cornerRadius: cornerRadius, corners: corners))
-    }
 }
